500dd504d88ec6206ed9af86a2f029e7413694e3
ing.dacastillop <ing.dacastillop@gmail.com>
2023-09-06 08:30:54 -0500

    :hammer: Define config for build project

--- rollup.config.js
+++ rollup.config.js
@@ -1,18 +1,31 @@
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import typescript from '@rollup/plugin-typescript';
+
 export default {
   input: 'dist/esm/index.js',
   output: [
     {
-      file: 'dist/lib.js',
+      file: 'dist/es/lib.js',
       format: 'es',
       sourcemap: true,
       inlineDynamicImports: true
     },
     {
-      file: 'dist/lib.cjs.js',
+      file: 'dist/cjs/lib.cjs.js',
       format: 'cjs',
       sourcemap: true,
       inlineDynamicImports: true
     }
   ],
-  plugins: []
+  plugins: [
+    resolve(),
+    commonjs(),
+    typescript({
+      tsconfig: './tsconfig.json',
+      declaration: true,
+      declarationDir: 'dist',
+      include: ['node_modules/@rolster/typescript-types/index.d.ts']
+    })
+  ]
 };
