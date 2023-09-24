fc0ec38eab4216f5c7853e6c3569292aa918692d
ing.dacastillop <ing.dacastillop@gmail.com>
2023-09-24 10:20:01 -0500

    :bug: Fix bad behavior in the Partial Sealed class

--- src/sealed.ts
+++ src/sealed.ts
@@ -27,17 +27,13 @@ export class PartialSealed<R, V, T extends StateSealed<R>> {
     private value?: V
   ) {}
 
-  public when(resolver: Partial<T>, otherwise?: () => void): R {
+  public when(resolver: Partial<T>, otherwise?: () => void): Undefined<R> {
     const handler = resolver[this.key];
 
     if (otherwise) {
       otherwise();
     }
 
-    if (handler) {
-      return handler(this.value);
-    }
-
-    throw Error('PartialSealed class could not resolve call');
+    return handler ? handler(this.value) : undefined;
   }
 }
